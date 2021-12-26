c366153c245491c73a481feff0ea48c802c2f688
zhongtaoaki <zhongtaodylan@163.com>
2021-12-26 12:05:49 +0900

    类的定义

--- src/third/ObjectDemo.java
+++ src/third/ObjectDemo.java
@@ -1,5 +1,44 @@
 package third;
+/**
+ * 类的声明
+ * 类的声明是在类外，在package里
+ * 作为一个文件出现
+ * 一个文件可以出现多个类，但是尽量只写一个类
+ * 一个文件里的多个类只能出现一次public
+ * 类的声明： class 类名 {}
+ * 类名的命名遵循大驼峰原则
+ * 类里可以有成员变量（field，属性），声明在方法里的变量叫本地变量
+ * 成名变量的声明是 （访问修饰子） 数据类型 变量名 （可以有初始值）；
+ * 
+ * 类的使用
+ * 在类内使用（可以是在方法外，也可以是方法内）
+ * 类名 变量名（对象名称） = new 类名（）；
+ * new当且仅当在生成对象的时候使用
+ * 用对象.属性名的方式去使用类的属性
+ */
+
 
 public class ObjectDemo {
     
+    public static void main(String[] args) {
+        //抓猫
+
+        int i = 1;
+
+        //生成一只新的猫
+        Cat a = new Cat();
+        a.age = 1;
+        a.gender = true;
+        System.out.println(a.age);
+        System.out.println(a.gender);
+
+    }
+
 }
+
+class Cat{
+    int age;
+    boolean gender;
+}
+
+
