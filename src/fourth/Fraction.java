b5a36c72dc0e90b73b1ad4b2b835f46af55118d9
zhongtaoaki <zhongtaodylan@163.com>
2022-01-09 12:02:25 +0900

    课堂练习-分数

---
+++ src/fourth/Fraction.java
@@ -0,0 +1,52 @@
+package fourth;
+
+public class Fraction {
+
+    private int numerator;// 分子
+    private int denominator;// 分母
+
+    // 无参空构造
+    Fraction() {
+    }
+
+    // 有参构造，分母不为零
+    Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            System.out.println("分母不能为0");
+        } else {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+    }
+
+    // 乘法方法，返回乘法结果
+    public Fraction multiple(Fraction f) {
+        this.numerator *= f.numerator;
+        this.denominator *= f.denominator;
+        return this;
+    }
+
+    // 输出方式
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    // 约分
+    public Fraction reduction() {
+        int smaller = numerator > denominator ? denominator : numerator;
+        while (numerator % smaller != 0 || denominator % smaller != 0) {
+            smaller--;
+        }
+        denominator /= smaller;
+        numerator /= smaller;
+        return this;
+    }
+
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(3, 4);
+        System.out.println(f1.multiple(f2).reduction().toString());
+
+    }
+
+}
