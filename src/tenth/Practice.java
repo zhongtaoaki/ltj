4e519a4d96a979d8176b1f322e7db4852f12c4f4
zhongtaoaki <zhongtaodylan@163.com>
2022-02-20 12:06:06 +0900

    practice 的答案

--- src/tenth/Practice.java
+++ src/tenth/Practice.java
@@ -26,13 +26,36 @@ public class Practice {
 
 	static String toJson(Map<String, String> map) {
 		// TODO
-
-		return null;
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		map.keySet().stream().forEach(k ->{
+			sb.append("\"");
+			sb.append(k);
+			sb.append("\":\"");
+			sb.append(map.get(k));
+			sb.append(",");
+		});
+		sb.deleteCharAt(sb.length()-1);
+		sb.append("}");
+		return sb.toString();
 	}
 
 	static Map<String, String> toMap(String json) {
-		// TODO
-
-		return null;
+		
+		// {"admin":"admin","jack":"jack","satou":"123123"}
+		
+		Map<String, String> map = new HashMap<>();
+		
+		json = json.substring(1, json.length()-1);
+		String[] elements = json.split(",");
+		for (String element : elements) {
+			String[] subElements = element.split(":");
+			System.out.println(subElements[0]);
+			//map.put(subElements[0].substring(1,subElements[0].length()-1), subElements[1].substring(1,subElements[1].length()-1));
+			String key = subElements[0].substring(1,subElements[0].length()-1);
+			String value = subElements[1].substring(1,subElements[1].length()-1);
+			map.put(key, value);
+		}
+		return map;
 	}
 }
